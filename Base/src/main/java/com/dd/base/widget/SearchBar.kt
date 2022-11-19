9e8dbbef08a1b0a53804b47b4035a9482e3262f0
丁笛 <dingd@meixing.com>
2022-11-19 09:42:23 +0800

    首页

--- Base/src/main/java/com/dd/base/widget/SearchBar.kt
+++ Base/src/main/java/com/dd/base/widget/SearchBar.kt
@@ -14,16 +14,79 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dd.base.ui.theme.AppTheme
 
+@Composable
+fun SearchBarNotClickable(
+    hint: String,//输入框提示文本
+    onClick: (() -> Unit)//输入框点击事件，提供点击跳转到搜索页等定制功能,默认为空
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(AppTheme.colors.transparent)
+            .padding(10.dp),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .background(AppTheme.colors.divider,CircleShape)
+                .height(30.dp)
+                .fillMaxWidth()
+                .clip(CircleShape)
+                .clickable {
+                    onClick.invoke()
+                }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "搜索",
+                modifier = Modifier
+                    .padding(horizontal = 4.dp)
+                    .size(16.dp)
+            )
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 2.dp)
+                    .weight(1f), Alignment.CenterStart
+            ) {
+                if (text.isEmpty()) {
+                    Text(
+                        text = hint,
+                        style = TextStyle(color = AppTheme.colors.textSecondary)
+                    )
+                }
+            }
+            if (text.isNotEmpty()) {
+                IconButton(
+                    onClick = { text = "" },
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .size(16.dp)
+                ) {
+                    Icon(imageVector = Icons.Default.Clear, contentDescription = "清空搜索框")
+                }
+            }
+        }
+    }
+}
+
 
 @Composable
 fun SearchBar(
     hint: String,//输入框提示文本
-    onClick:(()->Unit)?=null//输入框点击事件，提供点击跳转到搜索页等定制功能,默认为空
+    onClick: (() -> Unit)? = null//输入框点击事件，提供点击跳转到搜索页等定制功能,默认为空
 ) {
     var text by remember {
         mutableStateOf("")
@@ -47,11 +110,18 @@ fun SearchBar(
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier.padding(4.dp)
                 ) {
-                    Icon(imageVector = Icons.Default.Search, contentDescription = "搜索", modifier = Modifier.padding(horizontal = 4.dp).size(16.dp))
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "搜索",
+                        modifier = Modifier
+                            .padding(horizontal = 4.dp)
+                            .size(16.dp)
+                    )
                     Box(
                         Modifier
                             .fillMaxWidth()
-                            .padding(vertical = 2.dp).weight(1f), Alignment.CenterStart
+                            .padding(vertical = 2.dp)
+                            .weight(1f), Alignment.CenterStart
                     ) {
                         if (text.isEmpty()) {
                             Text(
@@ -62,7 +132,12 @@ fun SearchBar(
                         innerTextField()
                     }
                     if (text.isNotEmpty()) {
-                        IconButton(onClick = { text = "" }, modifier = Modifier.padding(horizontal = 4.dp).size(16.dp)) {
+                        IconButton(
+                            onClick = { text = "" },
+                            modifier = Modifier
+                                .padding(horizontal = 4.dp)
+                                .size(16.dp)
+                        ) {
                             Icon(imageVector = Icons.Default.Clear, contentDescription = "清空搜索框")
                         }
                     }
