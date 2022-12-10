9eb887d966bb7dd06a11a428b99385571deffae7
丁笛 <466820812@qq.com>
2022-12-10 19:06:16 +0800

    123

--- Base/src/main/java/com/dd/base/utils/JsonUtils.kt
+++ Base/src/main/java/com/dd/base/utils/JsonUtils.kt
@@ -7,9 +7,8 @@ class JsonUtils {
     companion object{
        val jsonDecoder by lazy {
            Json {
-               coerceInputValues = true
-               ignoreUnknownKeys = true
-               explicitNulls = false
+               ignoreUnknownKeys = true // JSON和数据模型字段可以不匹配
+               coerceInputValues = true // 如果JSON字段是Null则使用默认值
            }
        }
     }
