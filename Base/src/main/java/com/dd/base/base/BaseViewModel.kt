cee018e0f72031e5593118afe54e0bea590f0ec3
丁笛 <dingd@meixing.com>
2022-12-13 14:18:44 +0800

    优化并增加工厂类

--- Base/src/main/java/com/dd/base/base/BaseViewModel.kt
+++ Base/src/main/java/com/dd/base/base/BaseViewModel.kt
@@ -1,8 +1,20 @@
 package com.dd.base.base
 
+import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 open class BaseViewModel:ViewModel() {
 
 
+}
+
+@Composable
+inline fun <reified T :ViewModel> viewModelInstance(crossinline create:()-> T):T{
+    return viewModel(factory = object : ViewModelProvider.Factory{
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return create() as T
+        }
+    }) as T
 }
